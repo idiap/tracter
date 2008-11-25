b1d621e4a10195eaa870f91c5eda7bc6aa1a6355
Phil Garner <pgarner@iee.org>
2008-11-25 21:32:28 +0100

    Sound file tester.

---
+++ src/snddump.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2008 by IDIAP Research Institute
+ *                   http://www.idiap.ch
+ *
+ * See the file COPYING for the licence associated with this software.
+ */
+
+#include <cstdio>
+
+#include "SndFileSource.h"
+#include "ScreenSink.h"
+
+using namespace Tracter;
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+
+    SndFileSource* source = new SndFileSource();
+    ScreenSink sink(source);
+
+    source->Open(argv[1]);
+    sink.Open();
+
+    return 0;
+}
