967f8a5299a86ef7c2312ed70be871e92aef0cbb
Vincent Wan <vinny@juniper.dcs.shef.ac.uk>
2009-03-05 12:11:57 +0000

    Added HTKLibSource to make full use of HTKLib's functionality Removed initialisation of HTK from HCopyWrapper. HTK initialisation should be handled in main() not here in tracter.        new file:   src/HTKLibSource.cpp        new file:   src/HTKLibSource.h        modified:   src/ASRFactory.cpp        modified:   src/ASRFactory.h        modified:   src/HCopyWrapper.cpp        modified:   src/Makefile.am

---
+++ src/HTKLibSource.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2008 by The University of Sheffield
+ *
+ * Copyright 2008 by Idiap Research Institute
+ *                   http://www.idiap.ch
+ *
+ * See the file COPYING for the licence associated with this software.
+ */
+
+/*
+ * HTKLibSource.cpp - Use HTKLib as the source in a tracter plugin.
+ *
+ * This version reads files using HTKLib. The resulting features are specified by the HTK config file.
+ *
+ * Note that the setting of mArraySize must be set correctly. There is no access to any model files
+ * here so there is no easy way of getting it automatically.
+ *
+ *  Created on: 04-Mar-2009
+ *      Author: Vincent Wan, University of Sheffield, UK.
+ */
+
+#include "HTKLibSource.h"
+
+#include "HMath.h"
+#include "HSigP.h"
+#include "HVQ.h"
+#include "HLabel.h"
+#include "HModel.h"
+#include "esignal.h"
+#ifdef UNIX
+#include <sys/ioctl.h>
+#endif
+
+#undef FALSE
+#undef TRUE
+
+
+/*
+ * STACKSIZE as defined in HCopy
+ */
+#define STACKSIZE 100000        /* assume ~100K wave files */
+
+/**
+ * HTK initialisation should happen only once so we don't do it here. Initialise HTK elsewhere.
+ */
+Tracter::HTKLibSource::HTKLibSource(const char* iObjectName)
+    : CachedPlugin<float>()
+{
+    /***********************************************
+     * First sort out tracter specific/non-HTK stuff
+     ***********************************************/
+    mObjectName = iObjectName;
+    mArraySize = GetEnv("ArraySize", 39);
+    mSampleFreq = GetEnv("SampleFreq", 8000.0f);
+    mSamplePeriod = GetEnv("SamplePeriod", 80);
+
+    /***********************************************
+     * Now sort out the HTK specific stuff
+     ***********************************************/
+    /*
+     * Allocate an HTK heap iStack
+     */
+    CreateHeap(&iStack, (char*)"InBuf", MSTAK, 1, 0.0, STACKSIZE, LONG_MAX);
+}
+
+
+/**
+ * Maps the HTK parameter file and reads the header
+ */
+void Tracter::HTKLibSource::Open(const char* iFileName)
+{
+    FileFormat dfmt=UNDEFF;    /* Data input file format */
+
+    if((pbuf = OpenBuffer(&iStack,(char*)iFileName,50,dfmt,TRI_UNDEF,TRI_UNDEF))==NULL)
+        HError(3250,"ProcessFile: Config parameters invalid");   
+
+    /*
+     * Make the Observation structure for storing feature vectors. 
+     * We assume that mArraySize is correct because we have no easy access to a model set to get the correct number automatically.
+     */
+    BufferInfo pbinfo;
+    GetBufferInfo(pbuf,&pbinfo);
+    int swidth0=1;
+    short swidth[SMAX];
+    Boolean saveAsVQ = FALSE;
+    Boolean eSep;
+    ZeroStreamWidths(swidth0,swidth);
+    SetStreamWidths(pbinfo.tgtPK,mArraySize,swidth,&eSep);
+    data=MakeObservation(&iStack,swidth,pbinfo.tgtPK,saveAsVQ, eSep);
+
+    StartBuffer(pbuf);
+}
+
+/*
+ * Destructor
+ */
+Tracter::HTKLibSource::~HTKLibSource() throw() {
+    CloseBuffer( pbuf );
+    DeleteHeap( &iStack );
+}
+
+/**
+ * The Fetch call: EVIL HACK: We can ignore iIndex because HTK can parse it's own extended filenames to 
+ * seek to various positions in the file
+ */
+int Tracter::HTKLibSource::Fetch(IndexType iIndex, CacheArea& iOutputArea)
+{
+    int i;
+    int offset = iOutputArea.offset;
+    for (i=0; i<iOutputArea.Length(); i++)
+    {
+        if ( BufferStatus( pbuf ) == PB_CLEARED ) 
+            break;
+        if ( ! ReadAsBuffer( pbuf, &data ) )
+            break;
+        if (i == iOutputArea.len[0])
+            offset = 0;
+        float* cache = GetPointer(offset);
+        for (int stream = 1 ; stream <= data.swidth[0] ; stream++)
+        {
+            memcpy(cache,data.fv[stream]+1,data.swidth[stream]*sizeof(float));
+            cache+=data.swidth[stream];
+        }
+            /*
+             * Print the feature vectors to STDOUT for debugging purposes.
+             */
+             /*
+                printf("HTKLibSource:  ");
+                cache = GetPointer(offset);
+                for (int i=0; i<mArraySize; i++)
+                printf("%.3f ",cache[i]);
+                printf("\n");
+              */
+        iIndex++;
+        offset++;
+    }
+    return i;
+}
+
---
+++ src/HTKLibSource.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2008 by The University of Sheffield
+ *
+ * Copyright 2008 by Idiap Research Institute
+ *                   http://www.idiap.ch
+ *
+ * See the file COPYING for the licence associated with this software.
+ */
+
+/*
+ * HHTKLibSource.h
+ *
+ *  Created on: 01-Jul-2008
+ *      Author: Vincent Wan
+ */
+
+#ifndef HTKLIBSOURCE_H_
+#define HTKLIBSOURCE_H_
+
+/* As few headers as possible to allow the class to compile */
+#include "HShell.h"
+#include "HMem.h"
+#include "HWave.h"
+#include "HAudio.h"
+#include "HParm.h"
+
+
+#include "CachedPlugin.h"
+#include "Source.h"
+
+
+namespace Tracter
+{
+    /**
+     * Plugin to deal with source files using HTK
+     */
+    class HTKLibSource : public CachedPlugin<float> , public Source
+    {
+    public:
+        HTKLibSource(const char* iObjectName = "HTKLibSource");
+        ~HTKLibSource() throw();
+        void Open(const char* iFileName);
+
+    protected:
+        /** Diverts basic time stamp requests to the Source base class */
+        virtual TimeType TimeStamp(IndexType iIndex)
+        {
+             return Source::TimeStamp(iIndex);
+        }
+
+    private:
+        MemHeap iStack;         ///< HTK MSTAK memory heap
+        ParmBuf pbuf;           ///< HTK buffer where HParm does all its work
+        Observation data;       ///< The place where HTK writes the
+                                ///  feature vector
+        virtual int Fetch(IndexType iIndex, CacheArea& iOutputArea);
+
+    };
+}
+
+#endif /* HTKLIBSOURCE_H_ */
