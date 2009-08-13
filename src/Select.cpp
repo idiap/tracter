3a0947dc3d10bf5e0d32cb482cfbd92bf51bce2c
Phil Garner <Phil.Garner@idiap.ch>
2009-08-13 18:22:54 +0200

    Tidy up Select component
    Remove the infinite cache request and shorten the code using
    UnaryRead.

--- src/Select.cpp
+++ src/Select.cpp
@@ -4,10 +4,6 @@
  * See the file COPYING for the licence associated with this software.
  */
 
-//#include <cstdio>
-//#include <cmath>
-//#include <cfloat>
-
 #include "Select.h"
 
 Tracter::Select::Select(Plugin<float>* iInput, const char* iObjectName)
@@ -16,36 +12,28 @@ Tracter::Select::Select(Plugin<float>* iInput, const char* iObjectName)
     mObjectName = iObjectName;
 
     mLoIndex = GetEnv("Lo", 0);
-    mHiIndex = GetEnv("Hi", -1);
-
-    if (mHiIndex < 0)
-    {
-    	mHiIndex = iInput->GetArraySize() - 1;
-    }
+    mHiIndex = GetEnv("Hi", iInput->GetArraySize()-1);
 
     mArraySize = mHiIndex - mLoIndex + 1;
     assert(mArraySize >= 0);
-    assert(mHiIndex <= iInput->GetArraySize() - 1);
+    assert(mHiIndex < iInput->GetArraySize());
     assert(mLoIndex >= 0);
 
-    // Keep all the input
-    PluginObject::MinSize(iInput, 1);
+    MinSize(iInput, 1);
 }
 
 bool Tracter::Select::UnaryFetch(IndexType iIndex, int iOffset)
 {
     assert(iIndex >= 0);
 
-    CacheArea inputArea;
-    if (mInput->Read(inputArea, iIndex) != 1){
-      return false;
-    }
-
-    // Copy input to output with limits check
-    float* input  = mInput->GetPointer(inputArea.offset);
     float* output = GetPointer(iOffset);
+    const float* input = mInput->UnaryRead(iIndex);
+    if (!input)
+        return false;
+
+    // Copy input to output
     for (int i=mLoIndex; i<=mHiIndex; i++){
-      output[i-mLoIndex] = input[i];
+        output[i-mLoIndex] = input[i];
     }
 
     return true;
