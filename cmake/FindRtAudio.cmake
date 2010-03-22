c203a435fe4a3f084cd6aa2317cb86269ed3f44c
Phil Garner <Phil.Garner@idiap.ch>
2010-03-22 15:04:33 +0100

    More CMake; add packaging.

--- cmake/FindRtAudio.cmake
+++ cmake/FindRtAudio.cmake
@@ -5,9 +5,17 @@ set(RTAUDIO_DIR $ENV{RTAUDIO_DIR}
   CACHE FILEPATH "Path to RtAudio directory"
   )
 
+# Use the static library so it gets incorporated in the built library
+# If RtAudio is "package" this is less of an issue
 if (EXISTS "${RTAUDIO_DIR}")
   message(STATUS "Using RtAudio dir: ${RTAUDIO_DIR}")
   set(RTAUDIO_FOUND 1)
+  set(RTAUDIO_INCLUDE_DIRS
+    ${RTAUDIO_DIR}
+    )
+  set(RTAUDIO_LIBRARIES
+    ${RTAUDIO_DIR}/librtaudio.a
+    )  
 else (EXISTS "${RTAUDIO_DIR}")
   message(STATUS "RtAudio not found")
   set(RTAUDIO_FOUND 0)
