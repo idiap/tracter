8807356e492429812bdebe85b209102ca6d06504
Phil Garner <pgarner@gmx.ch>
2015-12-01 19:08:13 +0100

    Update ALSA and libresample finder functions
    ALSA is in cmake by default so delete it.
    libresample is in Debian and Arch, so find it via normal search.

--- cmake/FindALSA.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Find ALSA
-#
-# Phil Garner
-# March 2009
-#
-include(FindPkgConfig)
-
-pkg_check_modules(ALSA alsa)
--- cmake/FindLibResample.cmake
+++ cmake/FindLibResample.cmake
@@ -1,30 +1,29 @@
 #
-# Find LibResample
+# Copyright 2015 by Idiap Research Institute
 #
-set(LIBRESAMPLE_DIR $ENV{LIBRESAMPLE_DIR}
-  CACHE FILEPATH "Path to libresample directory"
-  )
+# See the file COPYING for the licence associated with this software.
+#
+# Author(s):
+#   Phil Garner, November 2015
+#
 
-if (EXISTS "${LIBRESAMPLE_DIR}")
-  message(STATUS "Using libresample dir: ${LIBRESAMPLE_DIR}")
-  set(LIBRESAMPLE_FOUND 1)
-  set(LIBRESAMPLE_INCLUDE_DIRS
-    ${LIBRESAMPLE_DIR}/src
-    ${LIBRESAMPLE_DIR}/include
-    )
-  set(LIBRESAMPLE_LIBRARIES
-    ${LIBRESAMPLE_DIR}/libresample.a
-    )
-endif (EXISTS "${LIBRESAMPLE_DIR}")
+find_path(
+  LIBRESAMPLE_INCLUDE_DIR libresample.h
+)
 
-if (EXISTS "/usr/lib/libresample.a")
-  set(LIBRESAMPLE_FOUND 1)
-  set(LIBRESAMPLE_INCLUDE_DIRS /usr/include)
-  set(LIBRESAMPLE_LIBRARIES /usr/lib/libresample.a)
-  message(STATUS "Using libresample from system")
-endif (EXISTS "/usr/lib/libresample.a")
+find_library(
+  LIBRESAMPLE_LIBRARY NAMES libresample.a
+)
 
-if (NOT LIBRESAMPLE_FOUND)
-  message(STATUS "libresample not found")
-  set(LIBRESAMPLE_FOUND 0)
-endif (NOT LIBRESAMPLE_FOUND)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  libresample
+  REQUIRED_VARS LIBRESAMPLE_LIBRARY LIBRESAMPLE_INCLUDE_DIR
+)
+
+if(LIBRESAMPLE_FOUND)
+  set(LIBRESAMPLE_LIBRARIES ${LIBRESAMPLE_LIBRARY})
+  set(LIBRESAMPLE_INCLUDE_DIRS ${LIBRESAMPLE_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(LIBRESAMPLE_INCLUDE_DIR LIBRESAMPLE_LIBRARY)
